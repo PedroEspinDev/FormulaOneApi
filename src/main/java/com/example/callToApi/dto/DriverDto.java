a1e37213ec5627eea1284ab82ded662ca5304216
PedroEspin <108736303+PedroEspinDev@users.noreply.github.com>
2023-11-20 20:04:52 +0100

    Update DriverDto.java

--- src/main/java/com/example/callToApi/dto/DriverDto.java
+++ src/main/java/com/example/callToApi/dto/DriverDto.java
@@ -5,15 +5,15 @@ import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
-<<<<<<< HEAD
+
 
 public class DriverDto extends Driver {
-=======
+
 @SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
 public class DriverDto {
->>>>>>> relations
+
 
     private Integer grands_prix_entered;
     private Integer world_championships;
